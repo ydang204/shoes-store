02292d372e70ab34b2ec13a881add1ace9513a28
ydang <ydang@futurify.vn>
2019-11-02 18:06:53 +0700

    [SS] Fix error in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,10 @@ import Landing from './components/pages/landing/landing';
 import Sample from './components/pages/sample/sample';
 import Footer from './components/shared/footer/footer';
 import Menu from './components/shared/menu/menu';
-import Header from './components/shared/header/header';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Header></Header>
       <Menu />
       <Landing></Landing>
       <Sample></Sample>
