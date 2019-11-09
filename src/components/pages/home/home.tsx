ce310754fdb0fe07cc6878cdb8d02cec89839f02
ydang <ydang@futurify.vn>
2019-11-10 01:30:48 +0700

    [SS-03] Create components for home page

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './home.scss';
+import "./home.scss";
+import MainSlider from "./main-slider/main-slider";
+import TrendingProducts from "./trending-products/trending-products";
+import NewProducts from "./new-products/new-products";
 
 type Props = {};
 
@@ -13,38 +16,10 @@ class Home extends React.Component<Props, States> {
 
   render() {
     return (
-      <div className="container">
-        <div className="row">
-        <div className="col-md-3 col-sm-6">
-            <div className="product-grid7">
-                <div className="product-image7">
-                    <a href="#">
-                        <img className="pic-1" src="http://bestjquery.com/tutorial/product-grid/demo8/images/img-1.jpg" />
-                        <img className="pic-2" src="http://bestjquery.com/tutorial/product-grid/demo8/images/img-2.jpg" />
-                    </a>
-                    <ul className="social">
-                        <li><a href="" className="fa fa-search"></a></li>
-                        <li><a href="" className="fa fa-shopping-bag" title="Xem chi tiết"></a></li>
-                        <li><a href="" className="fa fa-shopping-cart" title="Mua hàng"></a></li>
-                    </ul>
-                    <span className="product-new-label">
-                      <a href=""><i className="fa fa-heart-o"> 1</i></a>
-                    </span>
-                </div>
-                <div className="product-content">
-                  <div className="title">
-                    <h5><a href="#"> KINH MAT NU AH13</a></h5>
-                    <p>100,000đ</p>
-                  </div>
-                  <div>
-                    <button type="button" className="btn btn-outline-warning">
-                      LIÊN HỆ
-                    </button>
-                  </div>
-                </div>
-            </div>
-        </div>
-      </div>
+      <div className="home">
+        <MainSlider />
+        <TrendingProducts />
+        <NewProducts />
       </div>
     );
   }
---
+++ src/components/pages/home/trending-products/trending-products.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./trending-products.scss";
+
+interface Props {}
+
+const TrendingProducts: React.FC<Props> = props => {
+  return <div className="trending-products"></div>;
+};
+
+export default TrendingProducts;
