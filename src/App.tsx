24d8ea1ff7d84ff0be358f537e3a37e9769dbb4a
VoTanTra <travo9692@gmail.com>
2019-11-12 13:04:55 +0700

    ss-09 UI cart

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import './App.scss';
 import RouteLayout from './components/shared/layout/route-layout';
 import Home from './components/pages/home/home';
 import Login from './components/pages/auth/login/login';
+import Cart from './components/pages/cart/cart';
 
 const App: React.FC = () => {
   return (
@@ -16,6 +17,7 @@ const App: React.FC = () => {
           <Route>
             <RouteLayout  path="/login" component={Login} />
             <RouteLayout exact path="/" component={Home} />
+            <RouteLayout exact path="/cart" component={Cart} />
           </Route>
         </Switch>
       </BrowserRouter>
