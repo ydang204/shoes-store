f5b3a2096d3c51905f19fbeb855d20a59e656a23
DESKTOP-IU2AHER\DELL <trieuvi.dct1155@gmail.com>
2019-11-12 18:57:36 +0700

    Search implement

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,16 @@ import './App.scss';
 import RouteLayout from './components/shared/layout/route-layout';
 import Home from './components/pages/home/home';
 import Login from './components/pages/auth/login/login';
+import Search from './components/pages/search/search';
 
-const App: React.FC = () => {
+const App: React.FC = () => { 
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
           <Route>
-            <RouteLayout  path="/login" component={Login} />
+            <RouteLayout path="/login" component={Login} />
+            <RouteLayout path="/search" component={Search} />
             <RouteLayout exact path="/" component={Home} />
           </Route>
         </Switch>
--- src/components/shared/navbar/search-box/search-box.tsx
+++ src/components/shared/navbar/search-box/search-box.tsx
@@ -1,26 +1,53 @@
 import React from 'react';
 import { Typeahead } from 'react-bootstrap-typeahead';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import ReactDOM from 'react-dom';
 
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 import 'react-bootstrap-typeahead/css/Typeahead-bs4.css';
 import './search-box.scss';
 import { Nav } from 'reactstrap';
+import Search from '../../../pages/search/search'
+import { withRouter, RouteComponentProps } from 'react-router';
 
-type Props = {};
+
+interface Props extends RouteComponentProps {
+
+}
 
 type States = {
   data: [];
+  query: string
 };
 
 class SearchBox extends React.Component<Props, States> {
+
   constructor(props: Props) {
     super(props);
 
     this.state = {
-      data: []
+      data: [],
+      query: ''
     };
+  }
+
+  _handleKeyDown = ( e: any) => {
+
+    if (e.key == "Enter") {
+
+        this.props.history.push({
+          pathname: '/search',
+          search: this.state.query,
+          state: { query: this.state.query }
+        });
+    }
+  }
+
+  _handleInput = (queryString:string, e: any) => {
+
+    this.setState({query: queryString});
   }
 
   render() {
@@ -28,10 +55,13 @@ class SearchBox extends React.Component<Props, States> {
       <Nav navbar className="search-container">
         <div className="search-box">
           <FontAwesomeIcon icon={faSearch} />
+
           <Typeahead
             //   labelKey="name"
             options={this.state.data}
             placeholder="Tìm kiếm sản phẩm, shop....."
+            onInputChange={this._handleInput}
+            onKeyDown={this._handleKeyDown}
           />
         </div>
       </Nav>
@@ -39,4 +69,4 @@ class SearchBox extends React.Component<Props, States> {
   }
 }
 
-export default SearchBox;
+export default withRouter(SearchBox);
