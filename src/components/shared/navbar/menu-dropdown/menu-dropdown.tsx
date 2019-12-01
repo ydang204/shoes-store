07bc3e04549b1346ea3cd60d5fb89f45aa9f14e9
ydang <ydang@futurify.vn>
2019-12-01 15:26:17 +0700

    color default for dropdown menu toggle button

--- src/components/shared/navbar/menu-dropdown/menu-dropdown.tsx
+++ src/components/shared/navbar/menu-dropdown/menu-dropdown.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import './menu-dropdown.scss';
+import React from "react";
+import "./menu-dropdown.scss";
 
 import {
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
 } from "reactstrap";
 
 const MenuDropdown: React.FC = () => {
-    return (
-        <UncontrolledDropdown>
-            <DropdownToggle className="menu">
-                Trang phục
-            </DropdownToggle>
-            <DropdownMenu>
-                <DropdownItem header>Header</DropdownItem>
-                <DropdownItem disabled>Action</DropdownItem>
-                <DropdownItem>Another Action</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Another Action</DropdownItem>
-            </DropdownMenu>
-        </UncontrolledDropdown>
-    );
-}
+  return (
+    <UncontrolledDropdown>
+      <DropdownToggle className="menu" color="default">
+        Trang phục
+      </DropdownToggle>
+      <DropdownMenu>
+        <DropdownItem header>Header</DropdownItem>
+        <DropdownItem disabled>Action</DropdownItem>
+        <DropdownItem>Another Action</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Another Action</DropdownItem>
+      </DropdownMenu>
+    </UncontrolledDropdown>
+  );
+};
 export default MenuDropdown;
